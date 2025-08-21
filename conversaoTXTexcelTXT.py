ad077aa78b41925cee9e13fa85c02c84bb4bd3b4
Larissa Medeiros Couto Barcelos <larissa.barcelos@admin.uvv>
2025-08-21 17:08:49 -0300

    codigo inserindo tamanho das colunas manualmente finalizado a principio

--- conversaoTXTexcelTXT.py
+++ conversaoTXTexcelTXT.py
@@ -1,29 +1,41 @@
 import pandas as pd
 
-# Layout: nome da coluna no Excel + largura + alinhamento + preenchimento
+
+#Layout: nome da coluna no Excel + largura + alinhamento + preenchimento
 layout = [
-    {"col": "codigo", "length": 5, "align": "left", "fill": " "},
-    {"col": "nome",   "length": 20, "align": "left", "fill": " "},
-    {"col": "valor",  "length": 10, "align": "right", "fill": "0"},
+    # {"col": "EMPRESA", "length": 4, "align": "left", "fill": " "},
+    {"col": "COD LOCAL", "length": 5, "align": "right", "fill": " "},
+    {"col": "NOME DO LOCAL", "length": 25, "align": "right", "fill": " "},
+    {"col": "RESPONSAVEL",  "length": 18, "align": "right", "fill": " "},
+    {"col": "DESCRICAO DO LOCAL",  "length": 80, "align": "left", "fill": " "},
+    {"col": "PERMITE EMPRESTIMO",  "length": 2, "align": "left", "fill": " "},
+    {"col": "FILIAL",  "length": 4, "align": "right", "fill": " "},
+    {"col": "APLICACAO",  "length": 1, "align": "left", "fill": " "},
+    {"col": "SITUACAO",  "length": 1, "align": "left", "fill": " "},
 ]
 
 def format_field(value, length, align, fill):
     value = str(value) if pd.notna(value) else ""
+    value = value.replace("\r\n", " ").replace("\n", " ").replace("\r", " ")
+    value = " ".join(value.split())
+    
     if len(value) > length:
         value = value[:length]  # corta se passar
-    if align == "left":
-        return value.ljust(length, fill)
-    else:
-        return value.rjust(length, fill)
+        
+    return value.ljust(length, fill) if align == "left" else value.rjust(length, fill)
 
 def export_fixed_width(df, layout, output_file):
+    df.columns = df. columns.str.strip()
     with open(output_file, "w", encoding="utf-8") as f:
         for _, row in df.iterrows():
-            line = "".join(format_field(row[l["col"]], l["length"], l["align"], l["fill"]) for l in layout)
+            line = "".join(
+                format_field(row[l["col"]], l["length"], l["align"], l["fill"]) 
+                for l in layout)
             f.write(line + "\n")
 
 # Exemplo: lendo do Excel
-df = pd.read_excel("dados.xlsx")
+df = pd.read_excel("testelaryssa.xlsx")
 
-# Gera TXT no formato fixo
-export_fixed_width(df, layout, "saida.txt")
+# Gera TXT novo no formato fixo
+export_fixed_width(df, layout, "teste1.txt")
+print("txt gerado com sucesso!")
